47d2287d2a7e3b18c55791f72884665141dcde4c
Mariane Daniele <mdaniele2411@gmail.com>
2022-10-28 10:36:05 -0300

    Criando o componente time

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App(){
   const [colaboradores, setColaboradores] = useState([]);
@@ -11,6 +12,8 @@ function App(){
       <div className="App">
         <Banner />
         <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdd(colaborador)} />
+        <Time nome="Programação"/>
+        <Time nome="Front-end"/>
       </div>
     );
 }
---
+++ organo/src/componentes/Time/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Time.css'
+
+function Time(props){
+    return(
+        <section className='time'>
+            <h3>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Time
